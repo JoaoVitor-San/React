b8f7360abd3a80b00fb60b020b01794cc10f886d
João Vitor Santana <joao.sant.dev@gmail.com>
2024-12-20 00:01:44 -0300

    starting3

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,25 +1,45 @@
 import React from 'react';
 
 const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
+return (
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
         <a className="navbar-brand" href="#">
-          StoreUs
+        StoreUs
         </a>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
         >
         <span className="navbar-toggler-icon"></span>
-            </button>
+        </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link" href="#">
-                    Categoria 1            
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                        Categoria 1            
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            Categoria 2
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            Categoria 3
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    </div>
+</nav>
+);
+};
+
+export default NavBar;                    
