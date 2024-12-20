1847c8555930f98a07076e303999d1c9c3c87dd0
João Vitor Santana <joao.sant.dev@gmail.com>
2024-12-19 23:52:01 -0300

    starting1

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,3 +5,16 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#"></a>
+
+        StoreUs
+        
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
