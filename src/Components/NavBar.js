10df5295962c79d8dc208386ed52abd4c117de60
João Vitor Santana <joao.sant.dev@gmail.com>
2024-12-19 23:51:15 -0300

    starting

---
+++ src/Components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#"></a>
